ffc2463a27b1d4505cfd5d553e9fdbec5bdff595
dodikk <dodikk88.reg@gmail.com>
2013-12-11 13:38:57 +0200

    migrated to xctest

--- ESDotnetFormatter/ESDotnetFormatterTests/ESDotnetFormatterTests.m
+++ ESDotnetFormatter/ESDotnetFormatterTests/ESDotnetFormatterTests.m
@@ -1,8 +1,8 @@
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
 #import <ESDotnetFormatter/ESDotnetFormatter.h>
 
-@interface ESDotnetFormatterTests : SenTestCase
+@interface ESDotnetFormatterTests : XCTestCase
 @end
 
 @implementation ESDotnetFormatterTests
@@ -16,14 +16,14 @@
         expected = @"hello world";
         result = [ NSString stringWithDotnetFormat: @"hello world"
                                               args: nil ];
-        STAssertEqualObjects( result, expected, @"unused params must be ignored" );
+        XCTAssertEqualObjects( result, expected, @"unused params must be ignored" );
     }
     
     {
         expected = @"Hotel of California";
         result = [ NSString stringWithDotnetFormat: @"Hotel of California"
                                               args: @[ @"One", @"Two", @(3) ] ];
-        STAssertEqualObjects( result, expected, @"unused params must be ignored" );
+        XCTAssertEqualObjects( result, expected, @"unused params must be ignored" );
     }
 }
 
@@ -36,7 +36,7 @@
         expected = @"A quick brown fox jumps over a lazy dog";
         result = [ NSString stringWithDotnetFormat: @"A {0} brown fox {1} over a {2} dog"
                                               args: @[ @"quick", @"jumps", @"lazy" ] ];
-        STAssertEqualObjects( result, expected, @"unused params must be ignored" );
+        XCTAssertEqualObjects( result, expected, @"unused params must be ignored" );
     }
 }
 
@@ -49,7 +49,7 @@
         expected = @"A 42 fox 13 over a 666 dog";
         result = [ NSString stringWithDotnetFormat: @"A {0} fox {1} over a {2} dog"
                                               args: @[ @(42), @(13), @(666) ] ];
-        STAssertEqualObjects( result, expected, @"unused params must be ignored" );
+        XCTAssertEqualObjects( result, expected, @"unused params must be ignored" );
     }
 }
 
@@ -61,7 +61,7 @@
     
     result = [ NSString stringWithDotnetFormat: @"A {0} fox {1} over a {2} dog"
                                           args: @[ @(42), @(13), nowDate ] ];
-    STAssertNil( result, @"Dates are not supported" );
+    XCTAssertNil( result, @"Dates are not supported" );
 }
 
 -(void)testReordering
@@ -73,7 +73,7 @@
         expected = @"A quick brown dog jumps over a lazy fox";
         result = [ NSString stringWithDotnetFormat: @"A quick brown {1} jumps over a lazy {0}"
                                               args: @[ @"fox", @"dog" ] ];
-        STAssertEqualObjects( result, expected, @"unused params must be ignored" );
+        XCTAssertEqualObjects( result, expected, @"unused params must be ignored" );
     }
     
 }
@@ -87,7 +87,7 @@
         expected = @"A quick brown dog jumps over a lazy dog";
         result = [ NSString stringWithDotnetFormat: @"A quick brown {1} jumps over a lazy {1}"
                                               args: @[ @"fox", @"dog" ] ];
-        STAssertEqualObjects( result, expected, @"unused params must be ignored" );
+        XCTAssertEqualObjects( result, expected, @"unused params must be ignored" );
     }
     
 }
